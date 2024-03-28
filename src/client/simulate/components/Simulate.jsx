473dbdd9f1ec058818d31380660ddac9e1b56876
iamsanteri <alexander.liukkonen@gmail.com>
2024-03-28 14:48:46 +0200

    Test modified components

---
+++ src/client/simulate/components/Simulate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <p>
+      <b>☀️ This is sumulate ☀️</b>
+    </p>
+    <p>Not sure if this updates in real time.</p>
+    <p>Yes it does.</p>
+    <p>- Santeri Liukkonen</p>
+  </div>
+);
+
+export default About;
