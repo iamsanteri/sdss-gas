dea7744d1aae082e06afc38f94c6d6023e70a92c
iamsanteri <alexander.liukkonen@gmail.com>
2024-03-31 17:48:27 +0300

    Verified MUI functionality, installed icons. Noticed MUI theming using the provider approach only works in development, but not when deployed into production. Using global CSS for customization instead

--- src/client/simulate/components/Simulate.jsx
+++ src/client/simulate/components/Simulate.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import { AccessAlarm } from '@mui/icons-material';
 
 const About = () => (
   <div>
     <p>
-      <b>☀️ This is sumulate ☀️</b>
+      <b>☀️ This is simulate ☀️</b>
     </p>
     <p>Not sure if this updates in real time.</p>
     <p>Yes it does.</p>
+    <Button
+      variant="contained"
+      size="small"
+      startIcon={<AccessAlarm />}
+      disableElevation
+    >
+      Santerino
+    </Button>
     <p>- Santeri Liukkonen</p>
   </div>
 );
--- src/client/simulate/index.js
+++ src/client/simulate/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Simulate from './components/Simulate';
 
+import './styles.css';
+
 const container = document.getElementById('index');
 const root = createRoot(container);
 root.render(<Simulate />);
