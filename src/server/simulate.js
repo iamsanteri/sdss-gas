1d9cdb35303e1c9ab26f861daae0dcd0e458f855
iamsanteri <alexander.liukkonen@gmail.com>
2024-04-14 20:14:28 +0300

    Copy edits

--- src/server/simulate.js
+++ src/server/simulate.js
@@ -40,9 +40,9 @@ function writeUserNote(hiddenSheet, lastColumn) {
   const userNote = [
     'Automatic note:',
     'Each row is a single simulation run or a scenario. You can use the full output data on the left to draw desired figures or export it for visualization in other software.',
-    'If you need more than 1000 runs, you can rename this sheet and run new simulation while keeping your previous data.',
+    'If you need more than 1000 runs, you can rename this sheet and run a new simulation. This will keep your previous dat and create a new output sheet.',
     'Additional distributions or other functionality, as well as improved analytics and visualizations are coming soon and are planned to be released in the future.',
-    'If you have any feedback or would like to add something to the wishlist, please let me know by emailing me: santeri@simdss.com',
+    'If you have any feedback or would like to add something to the wishlist, please let me know by email: santeri@simdss.com',
   ];
 
   // Write each line of the user note to the spreadsheet, starting two cells to the right of the last column
